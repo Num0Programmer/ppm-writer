f15f1ffaa14d8c2ca542f6031b8d8d5db13178db
Num0Programmer <asajh4@gmail.com>
2023-10-18 02:09:04 -0700

    Finished implementing a function for the Header to struct to write to a PPM file at some path.

--- src/data.rs
+++ src/data.rs
@@ -1,10 +1,33 @@
+use std::{
+    fs::File,
+    io::{Write, Result}
+};
+
 /// Holds metadata for a PPM file
 pub struct Header
 {
     format: String,
     width: String,
     height: String,
-    max_color: String
+    max: String
+}
+
+impl Header
+{
+    pub fn write_buf(self, path: &str, buf: &Vec<u8>) -> Result<()>
+    {
+        let mut file = File::create(path)?;
+        writeln!(&mut file, "{}", self.format)?;
+        writeln!(&mut file, "{} {}", self.width, self.height)?;
+        writeln!(&mut file, "{}", self.max)?;
+
+        for byte in buf
+        {
+            writeln!(&mut file, "{}", byte)?;
+        }
+
+        Ok(())
+    }
 }
 
 impl Default for Header
@@ -16,7 +39,7 @@ impl Default for Header
             format: String::from("P3"),
             width: String::from("64"),
             height: String::from("64"),
-            max_color: String::from("255")
+            max: String::from("255")
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::env;
+use std::{env, io::Result};
 use data::Header;
 
-fn main()
+fn main() -> Result<()>
 {
-    let args: Vec<_> = env::args().collect();
+    // TODO: incorporate invokation arguments later
+    // let args: Vec<_> = env::args().collect();
 
     let header = Header::default();
+    let buf: Vec<u8> = vec![0; 12288];  // magic number for 64 * 64 * 3
+
+    header.write_buf("sample.ppm", &buf)?;
+
+    Ok(())
 }
